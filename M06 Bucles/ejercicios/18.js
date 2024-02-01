77681e9b036b674ca2e594fd4572d651a0edf5b4
L-Alvarenga <leoo.vdp@gmail.com>
2024-02-01 12:36:29 -0300

    Mod. 6 - Homeworks hasta 20 (18 y 20 sin resolver)

--- M06 Bucles/ejercicios/18.js
+++ M06 Bucles/ejercicios/18.js
@@ -1,7 +1,8 @@
+// Dados dos argumentos "a" y "b", devuelve el producto de todos
+// los números entre a y b (inclusive).
+// Tu código:
+
 function productoEntreNúmeros(a, b) {
-  // Dados dos argumentos "a" y "b", devuelve el producto de todos
-  // los números entre a y b (inclusive).
-  // Tu código:
 }
 
 module.exports = productoEntreNúmeros;
--- M06 Bucles/ejercicios/20.js
+++ M06 Bucles/ejercicios/20.js
@@ -1,8 +1,9 @@
+// La función recibe un numero n por argumento.
+// Devuelve la suma de todos los números desde 1 hasta n.
+// Si la suma supera a 100, detén el bucle usando break.
+// Tu código:
+
 function sumarHastaNConBreak(n) {
-  // La función recibe un numero n por argumento.
-  // Devuelve la suma de todos los números desde 1 hasta n.
-  // Si la suma supera a 100, detén el bucle usando break.
-  // Tu código:
 }
 
 module.exports = sumarHastaNConBreak;
